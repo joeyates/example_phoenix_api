e8a0d8c07bb4a0400dd6ac574654642c7e72df5e
Joe Yates <joe.g.yates@gmail.com>
2015-09-17 16:14:23 +0200

    Only insert valid data during tests

--- test/controllers/climb_controller_test.exs
+++ test/controllers/climb_controller_test.exs
@@ -2,7 +2,8 @@ defmodule PhoenixApi.ClimbControllerTest do
   use PhoenixApi.ConnCase
 
   alias PhoenixApi.Climb
-  @valid_attrs %{finish: 42, length: "120.5", name: "some content", start: 42}
+  @valid_attrs %{name: "A climb", start: 20, finish: 500, length: 12050.5}
+  @valid_attrs1 %{name: "A long climb", start: 30, finish: 600, length: 11000.5}
   @invalid_attrs %{}
 
   setup do
@@ -11,7 +12,7 @@ defmodule PhoenixApi.ClimbControllerTest do
   end
 
   test "lists all entries on index", %{conn: conn} do
-    climb = Repo.insert! %Climb{}
+    climb = Repo.insert!(Climb.changeset(%Climb{}, @valid_attrs))
     conn = get conn, climb_path(conn, :index)
     json = json_response(conn, 200)
     ids = Enum.map(json["data"], fn (c) -> c["id"] end)
@@ -19,7 +20,7 @@ defmodule PhoenixApi.ClimbControllerTest do
   end
 
   test "shows chosen resource", %{conn: conn} do
-    climb = Repo.insert! %Climb{}
+    climb = Repo.insert!(Climb.changeset(%Climb{}, @valid_attrs))
     conn = get conn, climb_path(conn, :show, climb)
     json = json_response(conn, 200)
     assert json["data"]["id"] == climb.id
@@ -44,20 +45,20 @@ defmodule PhoenixApi.ClimbControllerTest do
   end
 
   test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    climb = Repo.insert! %Climb{}
-    conn = put conn, climb_path(conn, :update, climb), climb: @valid_attrs
+    climb = Repo.insert!(Climb.changeset(%Climb{}, @valid_attrs))
+    conn = put conn, climb_path(conn, :update, climb), climb: @valid_attrs1
     assert json_response(conn, 200)["data"]["id"]
-    assert Repo.get_by(Climb, @valid_attrs)
+    assert Repo.get_by(Climb, @valid_attrs1)
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    climb = Repo.insert! %Climb{}
+    climb = Repo.insert!(Climb.changeset(%Climb{}, @valid_attrs))
     conn = put conn, climb_path(conn, :update, climb), climb: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "deletes chosen resource", %{conn: conn} do
-    climb = Repo.insert! %Climb{}
+    climb = Repo.insert!(Climb.changeset(%Climb{}, @valid_attrs))
     conn = delete conn, climb_path(conn, :delete, climb)
     assert json_response(conn, 200)["data"]["id"]
     refute Repo.get(Climb, climb.id)
